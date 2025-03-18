d23f0fc08233f2c5b916f81387eb948a1937bbf9
himarbg98 <163432611+himarbg98@users.noreply.github.com>
2025-03-18 19:18:34 +0100

    Update MavenFreeStyle.java

--- src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -15,10 +15,13 @@ public class MavenFreeStyle {
         System.out.println("Hello World!");
     }
     
-    public int fibonacci(int a){	
-	if (a >= 2){
-            return (fibonacci(a-1) + fibonacci(a-2));
+    public int fibonacci(int n){	
+	if (n <= 0){
+	    return 0;
+	} else if(n == 1) }
+	    return 1;
+        } else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
 	}
-	return 0;
     }
 }
